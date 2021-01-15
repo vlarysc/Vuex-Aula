0b345a6c0fe893fc117edd75d3b0d5b83e14943d
Vlary <manoelfranciscose@gmail.com>
2021-01-15 20:57:32 -0300

    aula

---
+++ src/components/Resumo.vue
@@ -0,0 +1,34 @@
+<template>
+    <Painel titulo="Resumo" roxo>
+        <div class="resumo">
+            <span>Total: <strong>{{ total | dinheiro }}</strong></span>
+            <hr>
+            <button>Finalizar!</button>
+        </div>
+    </Painel>
+</template>
+
+<script>
+export default {
+    computed: {
+        total() {
+            return this.produtos.map(p => p.quantidade * p.preco)
+                .reduce((total, atual) => total + atual, 0)
+        },
+        produtos() {
+            return this.$store.state.produtos
+        }
+    }
+}
+</script>
+
+<style>
+    table {
+        width: 100%;
+    }
+
+    td {
+        border-top: 1px solid #EEE;
+        width: 33%;
+    }
+</style>
